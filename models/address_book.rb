4ecfcd960cc2e33b98cde5c9fecdc3156998df18
Maley1993 <maley1993@gmail.com>
2018-01-27 22:08:15 -0700

    searching checkpoint complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,15 +1,17 @@
 require_relative 'entry'
 require 'csv'
-  class AddressBook
-    attr_reader :entries
 
-    def initialize
-      @entries = []
-    end
+class AddressBook
+
+  attr_reader :entries
+
+  def initialize
+    @entries = []
+  end
 
   def add_entry(name, phone_number, email)
     index = 0
-    entries.each do |entry|\
+    entries.each do |entry|
       if name < entry.name
         break
       end
@@ -18,14 +20,41 @@ require 'csv'
     entries.insert(index, Entry.new(name, phone_number, email))
   end
 
-   def import_from_csv(file_name)
-     csv_text = File.read(file_name)
-     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
-   # #8
-     csv.each do |row|
-       row_hash = row.to_hash
-       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+  end
+
+   # Search AddressBook for a specific entry by name
+  def binary_search(name)
+
+    lower = 0
+    upper = entries.length - 1
+
+     # #2
+     while lower <= upper
+       # #3
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+
+       # #4
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
      end
-   end
+
+     # #5
+     return nil
+  end
+
 
 end
